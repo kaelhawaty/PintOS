878ba1c4f0c5ed5f1de00604a34d60833673d93f
Karim elhawaty <kaelhawaty@gmail.com>
2020-12-27 21:21:19 +0200

    Minor fixes

--- userprog/process.h
+++ userprog/process.h
@@ -14,5 +14,7 @@ tid_t process_execute (const char *file_name);
 int process_wait (tid_t);
 void process_exit (void);
 void process_activate (void);
+unsigned hash_tid(const struct hash_elem *elem, void *aux UNUSED);
+unsigned child_cmp(const struct hash_elem *a, const struct hash_elem *b, void *aux UNUSED);
 
 #endif /* userprog/process.h */
